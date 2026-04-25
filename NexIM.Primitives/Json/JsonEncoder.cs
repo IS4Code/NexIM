ccea6bdfe61bf5390330b80214245226e9d95bf7
IS4 <illidans4@gmail.com>
2026-04-25 02:16:30 +0200

    unused

--- NexIM.Primitives/Json/JsonEncoder.cs
+++ NexIM.Primitives/Json/JsonEncoder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NexIM.Primitives.Json.Grammar;
